5b8452dc068c2a02d5d6c25c1234cb6a9b8b27c4
Marcela Ferreira <maferre.marcela@gmail.com>
2023-11-28 21:05:20 -0300

    Atualização

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -3,23 +3,35 @@
 2. Exibir o resultado na tela
 */
 
+const botaoSubtrair = document.getElementById("#subtrair")
+const botaoMultiplicar = document.getElementById("#multiplicar")
+
 function somar() {
  const numero1= parseFloat(document.querySelector("#numero1Soma").value)
  const numero2= parseFloat(document.querySelector("#numero2Soma").value)
 
- const resultado = numero1 + numero2
+ const total = numero1 + numero2
 
- const paragarafo = document.getElementById("resultadoSoma").innerHTML
- paragarafo.innerHTML = `Resultado: <strong> ${resultado}<strong/>`
+ document.querySelector(".resultado").setAttribute('id',"resultado")
+ 
 
- console.log(resultado)
+ const paragarafo = document.getElementById("resultadoSoma")
+ const resultado = document.createElement('strong')
+ paragarafo.appendChild(resultado)
+ resultado.innerText = total
 
 }
 
 
 
 function subtrair() {
+    const numero1= parseFloat(document.querySelector("#numero1Sub").value)
+    const numero2= parseFloat(document.querySelector("#numero2Sub").value)
+   
+    const resultado = numero1 - numero2
+    document.getElementById("resultadoSub").innerHTML = resultado
 
+    botaoSubtrair.addEventListener('click',subtrair)
 }
 
 function multiplicar() {
