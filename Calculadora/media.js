099a7de0300dd646fc25be462f7e95c9b5fb7c61
Marcela Ferreira <maferre.marcela@gmail.com>
2023-12-04 20:55:38 -0300

    calc media

--- Calculadora/media.js
+++ Calculadora/media.js
@@ -11,11 +11,14 @@
     6. Se a nota for entre 10, mostre na tela: 
       "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
 */
+
+const botaoMedia = document.getElementById("btnSituacao")
+
 function calcularMedia() {
-  const nota1 = paserFloat(document.getElementById('nota1').value)
-  const nota2 = paserFloat(document.getElementById('nota2').value)
-  const nota3 = paserFloat(document.getElementById('nota3').value)
-  const nota4 = paserFloat(document.getElementById('nota4').value)
+  const nota1 = parseFloat(document.getElementById('nota1').value)
+  const nota2 = parseFloat(document.getElementById('nota2').value)
+  const nota3 = parseFloat(document.getElementById('nota3').value)
+  const nota4 = parseFloat(document.getElementById('nota4').value)
 
 let media =(nota1+nota2+nota3+nota4)/4
 
@@ -23,13 +26,28 @@ let mensagem = ""
 
 if (media === 0 ){
   mensagem ="Infelizmente você zerou a prova :( "
-} 
+}
+ else if (media >= 0.1 && media <= 3){
+  mensagem = `Caramba, deu ruim, você obteve media ${media}! Estude mais e tente novamente!`
+}
+ else if (media >= 3.1 && media <= 5.9){
+  mensagem = `Você obteve media ${media}! Falta pouco para a média`
+}
+else if (media >= 6 && media <= 7){
+  mensagem = `Você está na média com ${media}`
+}
+else if (media >= 7.1 && media <= 9.9){
+  mensagem = `Notão! Sua média é ${media}`
+}
+else if (media >=10){
+  mensagem = "Hoje é seu aniversário? Pq você ta de parabéns! 10 de média"
+}
 
 const situacaoAluna = document.createElement('p')
-const divMedias = document.getElementById("contentCalMedias")
+const divMedias = document.getElementById("contentCalcMedias")
 divMedias.appendChild(situacaoAluna)
 situacaoAluna.innerText = mensagem
 
-
-
 }
+
+botaoMedia.addEventListener('click', calcularMedia)
